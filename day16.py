14561b6d48609fa23b092d81a8c16334c482cacf
gil <ga.bar@protonmail.com>
2018-12-16 15:10:32 -0600

    Day 16 part 2

--- day16.py
+++ day16.py
@@ -2,11 +2,9 @@ import re
 from collections import defaultdict
 from collections import Counter
 from itertools import dropwhile
-input = open('day16_input.txt').read().splitlines()
+inplines = open('day16_input.txt').read().splitlines()
 count = 0
 counter = defaultdict(Counter)
-
-#634 too low
 
 pat1 = re.compile(r'(\[.+\])')
 pat2= re.compile(r'(\d+)')
@@ -14,7 +12,7 @@ before = []
 opcode = []
 after = []
 
-for line_index, line in enumerate(input):    
+for line_index, line in enumerate(inplines):    
     if line == '':
         count+=1
         if count == 3:
@@ -22,7 +20,6 @@ for line_index, line in enumerate(input):
             break
         continue
     count = 0
-    
 
     if 'Before' in line:
             before.append(eval(pat1.findall(line)[0]))
@@ -31,151 +28,124 @@ for line_index, line in enumerate(input):
     else: 
         opcode.append([int(n) for n in pat2.findall(line)])
     
-def addr(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A] + before[B]
-    if before == after:
-        counter[addr][code]+=1
-        return True
+def addr(register, input):
+    A, B, C = input
+    register[C] = register[A] + register[B]
+    return register
 
-def addi(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A] + B
-    if before == after:
-        counter[addi][code]+=1
-        return True
+def addi(register, input):
+    A, B, C = input
+    register[C] = register[A] + B
+    return register
 
-def mulr(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A] * before[B]
-    if before == after:
-        counter[mulr][code]+=1
-        return True
+def mulr(register, input):
+    A, B, C = input
+    register[C] = register[A] * register[B]
+    return register
 
-def muli(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A] * B
-    if before == after:
-        counter[muli][code]+=1
-        return True
+def muli(register, input):
+    A, B, C = input
+    register[C] = register[A] * B
+    return register
 
-def banr(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A] & before[B]
-    if before == after:
-        counter[banr][code]+=1
-        return True
+def banr(register,input):
+    A, B, C = input
+    register[C] = register[A] & register[B]
+    return register
 
-def bani(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A] & B
-    if before == after:
-        counter[bani][code]+=1
-        return True
+def bani(register, input):
+    A, B, C = input
+    register[C] = register[A] & B
+    return register
 
-def borr(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A] | before[B]
-    if before == after:
-        counter[borr][code]+=1
-        return True
+def borr(register, input):
+    A, B, C = input
+    register[C] = register[A] | register[B]
+    return register
 
-def bori(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A] | B
-    if before == after:
-        counter[bori][code]+=1
-        return True
+def bori(register, input):
+    A, B, C = input
+    register[C] = register[A] | B
+    return register
 
-def setr(before, after, input):
-    code, A, B, C = input
-    before[C] = before[A]
-    if before == after:
-        counter[setr][code]+=1
-        return True
+def setr(register, input):
+    A, B, C = input
+    register[C] = register[A]
+    return register
 
-def seti(before, after, input):
-    code, A, B, C = input
-    before[C] = A
-    if before == after:
-        counter[seti][code]+=1
-        return True
+def seti(register, input):
+    A, B, C = input
+    register[C] = A
+    return register
 
-def gtir(before, after, input):
-    code, A, B, C = input
-    before[C] = int(A > before[B])
-    if before == after:
-        counter[gtir][code]+=1
-        return True
+def gtir(register, input):
+    A, B, C = input
+    register[C] = int(A > register[B])
+    return register
 
-def gtri(before, after, input):
-    code, A, B, C = input
-    before[C] = int(before[A] > B)
-    if before == after:
-        counter[gtri][code]+=1
-        return True
+def gtri(register, input):
+    A, B, C = input
+    register[C] = int(register[A] > B)
+    return register
 
-def gtrr(before, after, input):
-    code, A, B, C = input
-    before[C] = int(before[A] > before[B])
-    if before == after:
-        counter[gtrr][code]+=1
-        return True
+def gtrr(register, input):
+    A, B, C = input
+    register[C] = int(register[A] > register[B])
+    return register
 
-def eqir(before, after, input):
-    code, A, B, C = input
-    before[C] = int(A == before[B])
-    if before == after:
-        counter[eqir][code]+=1
-        return True
+def eqir(register, input):
+    A, B, C = input
+    register[C] = int(A == register[B])
+    return register
 
-def eqri(before, after, input):
-    code, A, B, C = input
-    before[C] = int(before[A] == B)
-    if before == after:
-        counter[eqri][code]+=1
-        return True
+def eqri(register, input):
+    A, B, C = input
+    register[C] = int(register[A] == B)
+    return register
 
-def eqrr(before, after, input):
-    code, A, B, C = input
-    
-    before[C] = int(before[A] == before[B])
-    if before == after:
-        counter[eqrr][code]+=1
-        return True
+def eqrr(register, input):
+    A, B, C = input
+    register[C] = int(register[A] == register[B])
+    return register
 
 samplecounter = 0
+
 for b, a, i in zip(before, after, opcode):
     opcounter = 0    
     for op in [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]:
-        
-        if op(b[:],a[:],i[:]):
+        r = b[:]
+        code, *input = i
+        if op(r, input) == a:
             opcounter +=1
+            counter[op][i[0]] +=1
     
     if opcounter >= 3:
         samplecounter+=1
 
-for op in counter:
-    print(op.__name__, counter[op])
+print(samplecounter)
 
 counters = sorted(list(counter.items()), key= lambda t: len(t[1]))
 
 assigned = []
 mapping = {}
 while len(assigned)< 16:
-    for op, counter in counters:
+    for op, counter in counters[:]:
         if len(counter) == 1:
             for c in counter:
                 assigned.append(c)
                 mapping[c] = op
+                counters.remove((op,counter))
 
         else:
             for k in counter.copy():
                 if k in assigned:
                     del counter[k]
 
-print(i)
+r = [0,0,0,0]
 
-for _, line in dropwhile(lambda t: t[0] < line_index, enumerate(input)):
-    opcode = [int(n) for n in pat2.findall(line)]
-    
+
+for _, line in dropwhile(lambda t: t[0] < line_index, enumerate(inplines)):    
+    opid, *in_ = [int(n) for n in pat2.findall(line)]
+    mapping[opid](r,in_)
+
+print(r)
